52490b04ba03cf2064c57697a31a5c1c76fdd313
Maja Vuevska <maja.vuevska@gmail.com>
2023-12-04 17:47:52 +0100

    Building the color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,17 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo-finki.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={40} />
-      <Text>Финки Документи</Text>
+    <HStack justifyContent="space-between" padding={10}>
+      <HStack>
+        <Image src={logo} boxSize={40} />
+        <Text>
+          Финки <br /> Документи
+        </Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 };
