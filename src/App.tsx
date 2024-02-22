61d1b5266af427b78ea9dc1b532e16eb51668073
Maja Vuevska <maja.vuevska@gmail.com>
2024-02-22 12:06:31 +0100

    Fix routing, add a reusable component for listing all or favourite documents

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,135 @@
 import {
-    Box,
-    Drawer,
-    DrawerBody,
-    DrawerContent,
-    DrawerOverlay,
-    Grid,
-    GridItem,
-    HStack,
-    Icon,
-    Show,
-    Switch,
-    useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Grid,
+  GridItem,
+  HStack,
+  Icon,
+  Show,
+  Stack,
+  useDisclosure,
 } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import CategoryList from "./components/CategoryList";
-import DocumentsTable from "./components/DocumentsTable";
-import {lazy, useState} from "react";
-import {Category} from "./hooks/useCategories";
-import DocumentHeading from "./components/DocumentHeading";
-import {HamburgerIcon} from "@chakra-ui/icons";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-const Favourites = lazy(() => import('./components/Favourites'));
-// import Favourites from "./components/Favourites";
+import NavBar from "./layout/NavBar";
+import CategoryList from "./layout/CategoryList";
+import { useState } from "react";
+import { Category } from "./hooks/useCategories";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import DocumentList from "./layout/DocumentList";
 
 export interface DocumentQuery {
-    category: Category | null;
-    searchText: string;
+  category: Category | null;
+  searchText: string;
 }
 
 function App() {
-    const [documentQuery, setDocumentQuery] = useState<DocumentQuery>(
-        {} as DocumentQuery
-    );
-    const {isOpen, onOpen, onClose} = useDisclosure();
+  const [documentQuery, setDocumentQuery] = useState<DocumentQuery>(
+    {} as DocumentQuery
+  );
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-
-    return (
-        <Grid
-            templateAreas={{
-                base: `"nav" "main" "footer"`,
-                lg: `"nav nav nav"
-             "aside main main"
-             "footer footer footer"`,
-            }}
-            templateColumns={{
-                base: "1fr",
-                lg: "300px 1fr",
-            }}
-        >
-            <GridItem area="nav" marginTop={3}>
-                <HStack>
-                    <Show below="lg">
-                        <Icon as={HamburgerIcon} onClick={onOpen} marginLeft={5}/>
-                        <Drawer placement={"left"} onClose={onClose} isOpen={isOpen}>
-                            <DrawerOverlay/>
-                            <DrawerContent>
-                                <DrawerBody paddingTop={5}>
-                                    <CategoryList
-                                        selectedCategory={documentQuery.category}
-                                        onSelectCategory={(category) =>
-                                            setDocumentQuery({...documentQuery, category})
-                                        }
-                                    ></CategoryList>
-                                </DrawerBody>
-                            </DrawerContent>
-                        </Drawer>
-                    </Show>
-
-                    <NavBar
-                        onSearch={(searchText) =>
-                            setDocumentQuery({...documentQuery, searchText})
-                        }
-                    ></NavBar>
-                </HStack>
-            </GridItem>
-            <Show above="lg">
-                <GridItem
-                    area="aside"
-                    margin={5}
-                    padding={10}
-                    border={1}
-                    borderRadius={10}
-                    borderColor="black.300"
-                >
+  return (
+    <Router>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" "footer"`,
+          lg: `"nav nav nav"
+                 "aside main main"
+                 "footer footer footer"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "300px 1fr",
+        }}
+      >
+        <GridItem area="nav" marginTop={3}>
+          <HStack>
+            <Show below="lg">
+              <Icon as={HamburgerIcon} onClick={onOpen} marginLeft={5} />
+              <Drawer placement={"left"} onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent>
+                  <DrawerBody paddingTop={5}>
                     <CategoryList
-                        selectedCategory={documentQuery.category}
-                        onSelectCategory={(category) =>
-                            setDocumentQuery({...documentQuery, category})
-                        }
-                    />
-                </GridItem>
+                      selectedCategory={documentQuery.category}
+                      onSelectCategory={(category) =>
+                        setDocumentQuery({ ...documentQuery, category })
+                      }
+                    ></CategoryList>
+                  </DrawerBody>
+                </DrawerContent>
+              </Drawer>
             </Show>
 
-            <GridItem area="main" margin={5} borderRadius={10}>
-                <DocumentHeading documentQuery={documentQuery}></DocumentHeading>
-                <DocumentsTable documentQuery={documentQuery}/>
-                <Router>
+            <NavBar
+              onSearch={(searchText) =>
+                setDocumentQuery({ ...documentQuery, searchText })
+              }
+            />
+          </HStack>
+        </GridItem>
+        <Show above="lg">
+          <GridItem
+            area="aside"
+            margin={5}
+            padding={10}
+            border={1}
+            borderRadius={10}
+            borderColor="black.300"
+          >
+            <CategoryList
+              selectedCategory={documentQuery.category}
+              onSelectCategory={(category) =>
+                setDocumentQuery({ ...documentQuery, category })
+              }
+            />
+          </GridItem>
+        </Show>
 
-                    <Routes>
-                      <Route path="/"  Component={Favourites}/>
-                      <Route path="/favourites" Component={Favourites}/>
-                      {/*<Route path="/categories" Component={CategoryList}/>*/}
+        <GridItem area="main" margin={5} borderRadius={10}>
+          <Routes>
+            <Route
+              path="/"
+              element={<DocumentList documentQuery={documentQuery} />}
+            />
+            <Route
+              path="/favourites"
+              element={
+                <DocumentList documentQuery={documentQuery} isFavorites />
+              }
+            />
+          </Routes>
+          <HStack marginLeft={10}>
+            <Link
+              to="/"
+              style={{
+                textDecoration: "underline",
+                marginRight: "10px",
+              }}
+            >
+              Сите
+            </Link>
+            <div>|</div>
+            <Link
+              to="/favourites"
+              style={{
+                textDecoration: "underline",
+                marginLeft: "10px",
+              }}
+            >
+              Омилени
+            </Link>
+          </HStack>
+        </GridItem>
 
-                    </Routes>
-                </Router>
-
-            </GridItem>
-
-            <GridItem area="footer" textAlign="center">
-                Footer
-            </GridItem>
-        </Grid>
-    );
+        <GridItem area="footer" textAlign="center">
+          Footer
+        </GridItem>
+      </Grid>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/layout/DocumentList.tsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Heading,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import axiosInstance from "../services/axios";
+import ActionButton from "../components/ActionButton";
+import { DocumentQuery } from "../App";
+
+interface Document {
+  id: number;
+  name: string;
+  description: string;
+  categoryId: number;
+}
+
+interface Props {
+  documentQuery: DocumentQuery;
+  isFavorites?: boolean;
+}
+
+const DocumentList: React.FC<Props> = ({
+  documentQuery,
+  isFavorites = false,
+}) => {
+  const [documents, setDocuments] = useState<Document[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get(
+          isFavorites ? "/favourites" : "/documents"
+        );
+        setDocuments(response.data);
+      } catch (error) {
+        console.error(
+          `Error fetching ${isFavorites ? "favorites" : "documents"}:`,
+          error
+        );
+        setError(`Error fetching ${isFavorites ? "favorites" : "documents"}.`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [isFavorites]);
+
+  const filteredDocuments = documentQuery.category
+    ? documents.filter((doc) => doc.categoryId === documentQuery.category?.id)
+    : documents;
+
+  if (isLoading) return <Spinner />;
+  if (error) return <Text>{error}</Text>;
+
+  return (
+    <>
+      <Heading fontSize={25} marginTop={10} marginLeft={10}>
+        {isFavorites ? "Омилени Документи" : "Документи"}
+      </Heading>
+
+      <TableContainer padding={10} marginTop={-5}>
+        <Table variant="striped" colorScheme="blue">
+          <Thead>
+            <Tr>
+              <Th>Document Name</Th>
+              <Th>Description</Th>
+              <Th colSpan={5} textAlign="center">
+                Actions
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {filteredDocuments.length === 0 ? (
+              <Tr>
+                <Td colSpan={3} textAlign="center" fontWeight="bold">
+                  Нема {isFavorites ? "омилени" : ""} документи во избраната
+                  категорија
+                </Td>
+              </Tr>
+            ) : (
+              filteredDocuments.map((doc) => (
+                <Tr key={doc.id}>
+                  <Td>{doc.name}</Td>
+                  <Td>{doc.description}</Td>
+                  <Td>
+                    <ActionButton
+                      documentId={doc.id}
+                      action={"view"}
+                      size={"sm"}
+                      padding={0}
+                    />
+                  </Td>
+                  <Td>
+                    <ActionButton
+                      documentId={doc.id}
+                      action={"edit"}
+                      size={"sm"}
+                      padding={0}
+                    />
+                  </Td>
+                  <Td>
+                    <ActionButton
+                      documentId={doc.id}
+                      action={"download"}
+                      size={"sm"}
+                      padding={0}
+                    />
+                  </Td>
+                  <Td>
+                    <ActionButton
+                      documentId={doc.id}
+                      action={"favourite"}
+                      size={"sm"}
+                      padding={0}
+                    />
+                  </Td>
+                  <Td>
+                    <ActionButton
+                      documentId={doc.id}
+                      action={"delete"}
+                      size={"sm"}
+                      padding={0}
+                    />
+                  </Td>
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+export default DocumentList;
