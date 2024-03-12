199b7f6ae029676e15c35fc041252391d9e80eb1
Nadica Dimitrovska <you@example.com>
2024-03-12 23:56:33 +0100

    Make the logos clickable

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,30 +1,33 @@
 import React from "react";
-import { Box, Flex, Image, Link } from "@chakra-ui/react";
-import { FaFacebook, FaTwitter, FaLinkedin, FaYoutube } from "react-icons/fa";
+import {Box, Flex, Image, Link} from "@chakra-ui/react";
+import {FaFacebook, FaTwitter, FaLinkedin, FaYoutube} from "react-icons/fa";
 
 const Footer = () => {
-  return (
-      <Box bg="gray.200" p={4} paddingRight={40} paddingLeft={40}>
-        <Flex align="center" justify="space-between">
-          <Image src="src/assets/finki_46.png" alt="Logo"  height={240} width={360}/>
+    return (
+        <Box bg="gray.200" p={4} paddingRight={40} paddingLeft={20}>
+            <Flex align="center" justify="space-between">
+                <Link href="https://www.finki.ukim.mk/mk" isExternal>
+                    <Image src="src/assets/finki_46.png" alt="Logo" height={240} width={360}/>
+                </Link>
 
-          <Flex>
-            <Link href="https://www.facebook.com/FINKI.ukim.mk/" isExternal>
-              <FaFacebook  fontSize="40px" mr={10}/>
-            </Link>
-            <Link href="https://twitter.com/FINKIedu" isExternal>
-              <FaTwitter fontSize="40px" mr={10} />
-            </Link>
-            <Link href="https://www.linkedin.com/school/faculty-of-computer-science-and-engineering---skopje/" isExternal>
-              <FaLinkedin fontSize="40px" mr={10} />
-            </Link>
-            <Link href="https://www.youtube.com/user/FINKIedu" isExternal>
-              <FaYoutube fontSize="40px" mr={10} />
-            </Link>
-          </Flex>
-        </Flex>
-      </Box>
-  );
+                <Flex>
+                    <Link href="https://www.facebook.com/FINKI.ukim.mk/" isExternal>
+                        <FaFacebook fontSize="40px" mr={10}/>
+                    </Link>
+                    <Link href="https://twitter.com/FINKIedu" isExternal>
+                        <FaTwitter fontSize="40px" mr={10}/>
+                    </Link>
+                    <Link href="https://www.linkedin.com/school/faculty-of-computer-science-and-engineering---skopje/"
+                          isExternal>
+                        <FaLinkedin fontSize="40px" mr={10}/>
+                    </Link>
+                    <Link href="https://www.youtube.com/user/FINKIedu" isExternal>
+                        <FaYoutube fontSize="40px" mr={10}/>
+                    </Link>
+                </Flex>
+            </Flex>
+        </Box>
+    );
 };
 
 export default Footer;
